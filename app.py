802669bb23c1db23b4737ca9ad3e18fce0b9aa55
BlueJillYang <articanon@foxmail.com>
2019-11-13 16:05:37 +0800

    modify the index

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+
+# configuration
+DEBUG = True
+
+# instantiate the app
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 
 @app.route('/')
@@ -8,5 +18,11 @@ def hello_world():
     return 'Welcome, darling!'
 
 
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run()
